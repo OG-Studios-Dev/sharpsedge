4c65df0b8c2101b1cc0289aa1784c3c362446f7e
Tony Soprano <tonysoprano.bot@gmail.com>
2026-03-17 16:20:07 -0400

    PGA Trends section: L5/L10 finishes + majors/course history

---
+++ src/lib/golf-trends-mock.ts
@@ -0,0 +1,18 @@
+export const MOCK_GOLF_TRENDS = {
+  scheffler: {
+    last5: ["1st", "3rd", "2nd", "1st", "T4"],
+    last10: { t10Plus: "65%", top5: "45%", cutsMade: "100%" },
+    majors: { masters: "T2 '24", pga: "5th '23", usOpen: "1st '22", open: "T8 '23" }
+  },
+  burns: {
+    last5: ["4th", "7th", "12th", "2nd", "5th"],
+    last10: { t20Plus: "60%", top10: "30%", cutsMade: "90%" },
+    courseHistory: { valspar: "1st '22, T5 '23" }
+  },
+  thomas: {
+    last5: ["8th", "3rd", "15th", "6th", "2nd"],
+    last10: { top10: "40%", cutsMade: "95%" },
+    majors: { masters: "2nd '19", pga: "4th '21", valsparWin: "'21" }
+  }
+  // +8 top field players
+};
